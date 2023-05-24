1a63489c54523dab4dd0db4e4bab86047e75dbaa
INFy93 <infy93@gmail.com>
2023-05-24 09:46:23 +0300

    [+] change credit logic for new API

--- user_credit.py
+++ user_credit.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from aiogram import types
 from redminelib import Redmine
 
-import tg_bot
+from menus import main_menu
 
 load_dotenv()
 
@@ -19,11 +19,13 @@ async def open_credit(uid, message: types.Message):
 
     req = requests.post(os.getenv("USER_CREDIT"), data=user_data)
 
-    if req.text != "-1":
-        await message.answer(req.text)
-    elif req.text == "-1":
+    credit_answer = req.json()
+
+    if credit_answer['status'] != -1:
+        await message.answer(credit_answer['message'])
+    elif credit_answer['status'] == -1:
         fail_button = types.InlineKeyboardMarkup()
         fail_button.add(types.InlineKeyboardButton("Попробовать еще раз",
-                                                   callback_data=tg_bot.usr_action_data.new
+                                                   callback_data=main_menu.menu_action_data.new
                                                    (action="here_my_knowledge", uid=uid)))
-        await message.answer("Ошибка параметров", reply_markup=fail_button)
+        await message.answer(credit_answer['message'], reply_markup=fail_button)
